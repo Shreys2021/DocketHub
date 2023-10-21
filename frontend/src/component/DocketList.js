26435e647796e4d229b6582976d6073363ad8635
Shreyas Asutkar <84585377+Shreys2021@users.noreply.github.com>
2023-10-21 18:29:22 +0530

    Update DocketList.js

--- frontend/src/component/DocketList.js
+++ frontend/src/component/DocketList.js
@@ -5,51 +5,60 @@ import { Link } from 'react-router-dom';
 
 const DocketList = () => {
     const [dockets, setDockets] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         axios
-            .get('https://dockethub.onrender.com/api/dockets')
+            .get('http://localhost:5000/api/dockets')
             .then((response) => {
                 setDockets(response.data);
+                setIsLoading(false);
             })
             .catch((error) => {
                 console.error('Error fetching Dockets:', error);
+                setIsLoading(false);
             });
     }, []);
 
     return (
         <div className="docket-container">
             <h2>Dockets</h2>
-            <ul>
-                {dockets.map((docket) => (
-                    <li key={docket._id} className="docket-item">
-                        <h3>{docket.name}</h3>
-                        <div className="docket-details">
-                            <p>
-                                <strong>Description:</strong> {docket.description}
-                            </p>
-                            <p>
-                                <strong>Start Time:</strong> {docket.startTime}
-                            </p>
-                            <p>
-                                <strong>End Time:</strong> {docket.endTime}
-                            </p>
-                            <p>
-                                <strong>Hours Worked:</strong> {docket.hoursWorked}
-                            </p>
-                            <p>
-                                <strong>Rate per Hour:</strong> {docket.ratePerHour}
-                            </p>
-                            <p>
-                                <strong>Supplier Name:</strong> {docket.supplierName}
-                            </p>
-                            <p>
-                                <strong>Purchase Order:</strong> {docket.purchaseOrder}
-                            </p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : dockets.length === 0 ? (
+                <p>No dockets found.</p>
+            ) : (
+                <ul>
+                    {dockets.map((docket) => (
+                        <li key={docket._id} className="docket-item">
+                            <h3>{docket.name}</h3>
+                            <div className="docket-details">
+                                <p>
+                                    <strong>Description:</strong> {docket.description}
+                                </p>
+                                <p>
+                                    <strong>Start Time:</strong> {docket.startTime}
+                                </p>
+                                <p>
+                                    <strong>End Time:</strong> {docket.endTime}
+                                </p>
+                                <p>
+                                    <strong>Hours Worked:</strong> {docket.hoursWorked}
+                                </p>
+                                <p>
+                                    <strong>Rate per Hour:</strong> {docket.ratePerHour}
+                                </p>
+                                <p>
+                                    <strong>Supplier Name:</strong> {docket.supplierName}
+                                </p>
+                                <p>
+                                    <strong>Purchase Order:</strong> {docket.purchaseOrder}
+                                </p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
             <Link to="/">Go back to Home</Link>
         </div>
     );
